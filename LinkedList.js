3327945854569844a9337925cfc83092b0c4295e
ColbyCabrera <gdthyispro@gmail.com>
2023-08-02 13:25:10 -0400

    Remove unnecessary code

--- LinkedList.js
+++ LinkedList.js
@@ -156,14 +156,3 @@ class Node {
     this.nextNode = null;
   }
 }
-
-let list = new LinkedList();
-
-list.append(1);
-list.append(2);
-list.append(3);
-list.append(4);
-
-console.log(list.toString());
-list.removeAt(1);
-console.log(list.toString());
